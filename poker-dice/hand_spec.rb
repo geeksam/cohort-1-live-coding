b49946bd0eb1a67badbcb5fbfc6e518b2f3f6fb9
Sam Livingston-Gray <geeksam@gmail.com>
2014-11-03 14:47:17 -0800

    Sketching out a single spec for Hand

---
+++ poker-dice/hand_spec.rb
@@ -0,0 +1,8 @@
+describe Hand do
+  it "has five dice, each of which has a known face value" do
+    dice = 5.times.map { LoadedDie.new('Q') }
+    hand = Hand.new( dice )
+    expect( hand.face_values ).to eq( %w[ Q Q Q Q Q ] )
+  end
+end
+
