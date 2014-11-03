a758c20794aa7d3c9bb945b51fa926610fd55be9
Sam Livingston-Gray <geeksam@gmail.com>
2014-11-03 11:57:22 -0800

    Remove duplication in divisibility checks

--- tdd-fizzbuzz/fizzbuzz.rb
+++ tdd-fizzbuzz/fizzbuzz.rb
@@ -2,11 +2,14 @@ module Fizzbuzz
   extend self
 
   def fb_value(n)
-    if (n % 3).zero? && (n % 5).zero?
+    divisible_by_three = (n % 3).zero?
+    divisible_by_five  = (n % 5).zero?
+
+    if divisible_by_three && divisible_by_five
       "FizzBuzz"
-    elsif (n % 3).zero?
+    elsif divisible_by_three
       "Fizz"
-    elsif (n % 5).zero?
+    elsif divisible_by_five
       "Buzz"
     else
       n
