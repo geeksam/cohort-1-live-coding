0ccfd336cbe2a68dfa67ac92789a3f0a5a8fab70
Sam Livingston-Gray <geeksam@gmail.com>
2014-11-03 15:36:17 -0800

    Make all the lines that create dice be consistent

--- poker-dice/hand_spec.rb
+++ poker-dice/hand_spec.rb
@@ -3,13 +3,13 @@ require_relative 'loaded_die'
 
 describe Hand do
   it "has five dice, each of which has a known face value" do
-    dice = 5.times.map { LoadedDie.new('Q') }
+    dice = %w[ Q Q Q Q Q ].map { |value| LoadedDie.new(value) }
     hand = Hand.new( dice )
     expect( hand.face_values ).to eq( %w[ Q Q Q Q Q ] )
   end
 
   specify "a Hand with five Queens is ranked as 'five of a kind'" do
-    dice = 5.times.map { LoadedDie.new('Q') }
+    dice = %w[ Q Q Q Q Q ].map { |value| LoadedDie.new(value) }
     hand = Hand.new( dice )
     expect( hand.rank ).to eq( 'Five of a kind' )
   end
