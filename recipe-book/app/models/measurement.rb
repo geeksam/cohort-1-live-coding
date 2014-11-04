cad71621fafa25179ddc8cf5772d85cd54682449
Sam Livingston-Gray <geeksam@gmail.com>
2014-11-04 15:10:11 -0800

    OMGWTF IT WORKS

--- recipe-book/app/models/measurement.rb
+++ recipe-book/app/models/measurement.rb
@@ -1,2 +1,4 @@
 class Measurement < ActiveRecord::Base
+  belongs_to :recipe
+  belongs_to :ingredient
 end
--- recipe-book/app/models/recipe.rb
+++ recipe-book/app/models/recipe.rb
@@ -3,9 +3,13 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :measurements
 
   def self.with_ingredient_named(ingredient_name)
-    all
+    Recipe.joins({:measurements => :ingredient})\
+      .where("'ingredients'.'name' = ?", ingredient_name)
   end
 
-  def add_ingredient(ingredient, attributes)
+  def add_ingredient(ingredient, measurement_attributes)
+    measurements.create(measurement_attributes) do |measurement|
+      measurement.ingredient = ingredient
+    end
   end
 end
